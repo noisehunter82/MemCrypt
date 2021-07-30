98aaaeb78ccbd21202a898c826bca65c08ec4a29
MacOS host <69931086+noisehunter82@users.noreply.github.com>
2021-07-30 20:20:35 +0100

    Refactor isKnownHeader. Close() files.

--- memcrypt_analysis.py
+++ memcrypt_analysis.py
@@ -1,11 +1,7 @@
 from Cryptodome.Cipher import AES
-from Cryptodome.Util.Padding import unpad
 from itertools import permutations
 import os
 import codecs
-
-
-# python3 memcrypt_analysis.py
 
 # Below are header values for common file types.
 JPEG_HDR = b'\xFF\xD8\xFF\xE0'
@@ -29,17 +25,17 @@ def isIncremental(buffer):
      Keyword arguments:
      buffer -- the buffer to check for incremental values. 16-byte size buffers are passed in by default.
     """
-    hex_buffer = codecs.encode(buffer, 'hex') # Encode ASCII to hex
-    hex_string = hex_buffer.decode('utf-8') # Transform into string literal
+    hex_buffer = codecs.encode(buffer, 'hex') # Encode ASCII to hex.
+    hex_string = hex_buffer.decode('utf-8') # Transform into string literal.
 
     int_array = []
-    list_of_indexes = list(range(0, 31, 2)) # Create list of 16 index values: 0,2,4,6....30
+    list_of_indexes = list(range(0, 31, 2)) # Create list of 16 index values: 0,2,4,6....30.
 
-    for i in list_of_indexes: # Extract values as 2 character strings, covert them to integers and append to a list
+    for i in list_of_indexes: # Extract values as 2 character strings, covert each to integer and append to a list.
         value = hex_string[i:i+2]
         int_array.append(int(value, 16))
 
-    if int_array == list(range(int_array[0], int_array[0]+16, 1)): # Detectd if the list of integers matches a list of incremental values where both start with the same value 
+    if int_array == list(range(int_array[0], int_array[0]+16, 1)): # Detect if the list of integers matches a list of incremental values where both start with the same value .
         return True
 
     return False
@@ -64,47 +60,47 @@ def decryptFile(candidates):
 
 
     file_in = open('data/encrypted_file', 'rb')
-    encrypted_data = file_in.read()  # Open the encrypted file and assign its content to a variable
+    encrypted_data = file_in.read()  # Opens the encrypted file and assigns its content to a variable.
     file_in.close()
     decrypted_data = ''
 
     for combination in permu:  # For every combination of key and iv...
         key = combination[0]
         iv = combination[1]
-        file_extension = []
-
+        
         cipher = AES.new(key, AES.MODE_CBC, iv)
         decrypted_data = cipher.decrypt(encrypted_data)  # ...use the current value of key and iv to decrypt ciphered data...
 
-        if isKnownHeader(decrypted_data, file_extension):  #  ...and verify if decrypted file has a known header. If it does...
+        # ...and verify if decrypted file has a known header. If it does...
+        is_known, file_extension = isKnownHeader(decrypted_data)
+
+        if is_known:  
           print(key, iv)  # Display correct combination of key and iv in terminal, and...
 
-          output_file = open('data/decrypted_file' + file_extension[0], 'xb')
-          output_file.write(decrypted_data) #...save the decrypted data as a new file with correct extension
+          output_file = open('data/decrypted_file' + file_extension, 'xb')
+          output_file.write(decrypted_data) #...save the decrypted data as a new file with correct extension.
+          output_file.close()
 
 
 
 
-def isKnownHeader(buffer, file_extension):
+def isKnownHeader(buffer):
     """This function performs analysis on the decrypted buffer to determine if it matches a known header (i.e. file type).
      If a match is detected, then it is likely the decryption process was successful.
 
      Keyword arguments: 
      buffer -- The buffer we wish to determine if decryption was successful.
     """
+    # If a valid file header is detected function returns a boolean and a string with correct file extension. 
     if JPEG_HDR in buffer[0:len(JPEG_HDR)]:
-        file_extension.append('.jpeg')
-        return True
+        return True, '.jpeg'
     if MS_OFFICE_HDR in buffer[0:len(MS_OFFICE_HDR)]:
-        file_extension.append('.docb')
-        return True
+        return True, '.docb'
     if PNG_HDR in buffer[0:len(PNG_HDR)]:
-        file_extension.append('.png')
-        return True
+        return True, '.png'
     if PDF_HDR in buffer[0:len(PDF_HDR)]:
-        file_extension.append('.pdf')
-        return True
-    return False
+        return True, '.pdf'
+    return False, ''
 
 
 def memoryAnalysis(file, offset):
@@ -125,7 +121,6 @@ def memoryAnalysis(file, offset):
 
             if isIncremental(read) == False:
                 candidates.append(read)
-                #print(read)
 
     return candidates
 
